76e975c6a93f223706fc3661e583d62142b50e99
Mohan Chaitanya <mohanchaitanya8@outlook.com>
2021-01-21 09:29:20 -0500

    Final Groot App

---
+++ script.js
@@ -0,0 +1,26 @@
+var inputElement = document.querySelector('#input-box')
+var buttonEvent = document.querySelector('#btn-translate');
+var outputData = document.querySelector('#output');
+
+var serverURL = "https://api.funtranslations.com/translate/groot.json";
+
+function translateURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Duh!! there is an error" + error);
+}
+
+function clickHandler() {
+    var inputText = inputElement.value;
+
+    fetch(translateURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            outputData.innerHTML = json.contents.text;
+        })
+        .catch(errorHandler);
+};
+
+buttonEvent.addEventListener('click', clickHandler);
