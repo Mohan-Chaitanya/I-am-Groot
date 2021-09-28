efc52fbc61b6c6c530292a0dd8418560fb76ea32
Mohan Chaitanya <mohanchaitanya8@outlook.com>
2021-09-28 05:18:43 -0400

    API Response logic update

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-var inputElement = document.querySelector('#input-box')
-var buttonEvent = document.querySelector('#btn-translate');
-var outputData = document.querySelector('#output');
+var inputElement = document.querySelector("#input-box");
+var buttonEvent = document.querySelector("#btn-translate");
+var outputData = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/groot.json";
 
 function translateURL(text) {
-    return serverURL + "?" + "text=" + text;
+  return serverURL + "?" + "text=" + text;
 }
 
 function errorHandler(error) {
-    console.log("Duh!! there is an error" + error);
+  console.log("Duh!! there is an error" + error);
 }
 
 function clickHandler() {
-    var inputText = inputElement.value;
-    console.log(inputText);
-    fetch(translateURL(inputText))
-        .then(response => response.json())
-        .then(json => {
-            outputData.innerHTML = json.contents;
-        })
-        .catch(errorHandler);
-};
+  var inputText = inputElement.value;
+  console.log(inputText);
+  fetch(translateURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      outputData.innerHTML = json.contents.translated;
+    })
+    .catch(errorHandler);
+}
 
-buttonEvent.addEventListener('click', clickHandler);
+buttonEvent.addEventListener("click", clickHandler);
